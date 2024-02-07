7ac9b0db2ecb2a15af319c90095fae7a1b575cf0
EAli07 <152880937+EAli07@users.noreply.github.com>
2024-02-07 10:59:55 +0000

    linked list add

--- listq.py
+++ listq.py
@@ -1,6 +1,10 @@
+# initialise variables to manage the list
 head = 0
+# used to support accessing the array elements to make it more readable
 data = 0
 pointer = 1
+# used to track size of list to check if empty or fulll
+maxCounter=4
 linkedlist = [
     ["Bob", 3],
     ["Sarah", 2],
@@ -9,24 +13,44 @@ linkedlist = [
     [None, None],
     [None, None]
 ]
-
-#check if list is empty
-if (linkedlist[head]) == None:
-    print("List empty)")
-else:
-    current = head
-    while current != None:
-        #start at the head
-        #print data of current node
-        print(linkedlist[current][data])
-        #go to pointer of current node
-        current = (linkedlist[current][pointer])
-        #if the head is none, list is empty
-        if current == None:
-            #if the pointer of the current node is none, then the list is finished
-            print("List is finished")
+# set the size of the list to begin
+maxSize = len(linkedlist)
 
 
+def traverseList():
+    global head, data, pointer, linkedlist
+    #check if list is empty
+    if (linkedlist[head]) == None:
+        print("List empty)")
+    else:
+        current = head
+        while current != None:
+            #start at the head
+            #print data of current node
+            print(linkedlist[current][data])
+            #go to pointer of current node
+            current = (linkedlist[current][pointer])
+            #if the head is none, list is empty
 
-#traversed the list now need to move onto adding a new node
-#potentially putting new node in order
+def addToEndOfList():
+    '''
+        check that there is free space in the array
+        traverse the list to find the end of the list (this will be the element whose next_position index pointer has the value Null)
+        store the new item in the free space found; this item is now the end of the list
+        adjust the next_position index pointer of the element that was previously last in the list
+    '''
+    global head, data, pointer, linkedlist
+    #  check that there is free space in the array
+    if maxCounter == maxSize:
+        print("Error: List is full")
+    else:
+        #traverse the list to find the end of the list (this will be the element whose next_position index pointer has the value Null)
+        #store the new item in the free space found; this item is now the end of the list
+        #adjust the next_position index pointer of the element that was previously last in the list
+        current = head
+        while current != None:
+            print(linkedlist[current][data])
+            current = (linkedlist[current][pointer])
+        
+  
+addToEndOfList()
